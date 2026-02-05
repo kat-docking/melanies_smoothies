e648370db29bc3d2c1e505ef0668858acc996746
kat-docking <katdocking07@gmail.com>
2026-02-05 15:14:00 +1030

    Update streamlit_app.py
    Adding smoothiefroot data

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,8 +31,6 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
-
-
     #st.write(ingredients_string)
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
@@ -45,3 +43,8 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
 
         st.success('Your Smoothie is ordered!', icon="✅")
+
+# New section to display smoothiefroot nutrition information
+import requests 
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
